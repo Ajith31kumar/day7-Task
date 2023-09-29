a8d435d0b4a2c6961491a87e19261c7d8abab66a
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-09-29 16:00:37 +0530

    Update usDollor.js

--- usDollor.js
+++ usDollor.js
@@ -1,19 +1,17 @@
+E)Print the country which uses US Dollars as currency.
+
 var request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
 request.onload = function () {
   var result = JSON.parse(request.response);
 
-  // Filter countries that use USD as a currency
   var usDollarCountries = result.filter(function (country) {
     return country.currencies && country.currencies.USD;
   });
-
-  // Extract and print the names of countries
   var usDollarCountryNames = usDollarCountries.map(function (country) {
   return country.name.common;
  });
-
   console.log("Countries using US Dollars as currency:");
   console.log(usDollarCountryNames);
 };
