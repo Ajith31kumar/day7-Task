53dc5cfcaa193f4b1052bfc58415196d2931317c
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-09-29 16:11:13 +0530

    Add files via upload

---
+++ popul lessthan2l.js
@@ -0,0 +1,12 @@
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    var result = JSON.parse(request.response);
+
+    var allcountries = result.filter(function(country){
+        return country.population < 200000;
+});
+    console.log(allcountries);
+
+}
