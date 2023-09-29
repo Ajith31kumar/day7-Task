ebfd231bfe82567e7411967ad22b28dac427bf71
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-09-29 15:58:22 +0530

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -3,11 +3,9 @@ request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
 request.onload = function () {
   var result = JSON.parse(request.response);
-
-  // Use reduce to calculate the total population
+  
   var totalPopulation = result.reduce(function (acc, country) {
     return acc + (country.population || 0); // Ensure population is a number
   });
-
   console.log("Total Population: " + totalPopulation);
 };
