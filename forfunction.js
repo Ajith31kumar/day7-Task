af1e88678d4af794635d7b0e8fcd07902298f263
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-09-29 15:56:58 +0530

    Update forfunction.js

--- forfunction.js
+++ forfunction.js
@@ -3,8 +3,6 @@ request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
 request.onload = function () {
   var result = JSON.parse(request.response);
-
-  // Use forEach to iterate through each country and print details
   result.forEach(function (country) {
     console.log("Name: " + country.name.common);
     console.log("Capital: " + (country.capital[0] || "N/A"));
