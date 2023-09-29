f4fb5114b53ef627d951b92226cc0cea9355fbfb
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-09-29 16:12:16 +0530

    Update popul lessthan2l.js

--- popul lessthan2l.js
+++ popul lessthan2l.js
@@ -1,3 +1,5 @@
+C) Get all the countries with a population of less than 2 lakhs using Filter function
+
 var request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all");
 request.send();
