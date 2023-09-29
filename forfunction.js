05073265b29daf8f218131210fff219b4bcff3b7
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-09-29 16:06:03 +0530

    Update forfunction.js

--- forfunction.js
+++ forfunction.js
@@ -1,3 +1,5 @@
+B)Print the following details name, capital, flag using forEach function
+
 var request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v3.1/all");
 request.send();
